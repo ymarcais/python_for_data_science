7978b7a94fe5587aeaea660ebae1c3cbbe6f5a27
ymarcais <ymarcais@gmail.com>
2023-06-04 17:36:59 +0200

    ex_01

--- day_3/S1E7.py
+++ day_3/S1E7.py
@@ -3,9 +3,11 @@ from S1E9 import Character
 class Baratheon(Character):
     """Derived class representing a Baratheon character."""
     
-    family_name = "Baratheon"
-    eyes = "brown"
-    hairs = "dark"
+    def __init__(self, first_name, family_name = "Baratheon", eyes = "brown", hairs = "dark"):
+        super().__init__(first_name = first_name, is_alive = True)
+        self.family_name = family_name 
+        self.eyes = eyes
+        self.hairs = hairs
     
     def die(self):
         """Method to change the health state of the Baratheon character to False."""
@@ -17,16 +19,17 @@ class Baratheon(Character):
     
     def __repr__(self):
         """String representation of the Baratheon character."""
-        return f"Character: {self.first_name}"
+        return f"<bound method {self.family_name}.__repr__ of Vector: ({self.family_name}, {self.eyes}, {self.hairs})>"
     
-
 class Lannister(Character):
     """Derived class representing a Lannister character."""
-    def __init__(self, my_variable):
-        self.family_name = "Lannister"
-        self.eyes = "blue"
-        self.hairs = "light"
-    
+    def __init__(self, first_name, family_name = "Lannister", eyes = "blue", hairs = "light"):
+        super().__init__(first_name = first_name, is_alive = True)
+        self.family_name = family_name 
+        self.eyes = eyes
+        self.hairs = hairs
+        
+        
     def die(self):
         """Method to change the health state of the Lannister character to False."""
         self.is_alive = False
@@ -35,9 +38,10 @@ class Lannister(Character):
         """String representation of the Lannister character."""
         return f"Name: {self.first_name}, Family: {self.family_name}, Eyes: {self.eyes}, Hairs: {self.hairs}, Alive: {self.is_alive}"
     
+    
     def __repr__(self):
         """String representation of the Lannister character."""
-        return f"Character: {self.first_name}"
+        return f"<bound method {self.family_name}.__repr__ of Vector: ({self.family_name}, {self.eyes}, {self.hairs})>"
     
     @classmethod
     def create_lannister(cls, first_name, is_alive=True):
